7389579dca8d97beecbb98f486a4648527c96eca
AlefV <planeado@gmail.com>
2019-02-01 17:40:44 +0300

    Добавлены два теста класса Point для расстояния между точками

---
+++ Sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+    @Test
+    public void pointTest1(){
+        Point a = new Point(1,3);
+        Point b = new Point (1,4);
+        Assert.assertEquals(a.distance(a, b),1.0);
+    }
+    @Test
+    public void pointTest2(){
+        Point a = new Point(1,3);
+        Point b = new Point (2,3);
+        Assert.assertEquals(a.distance(a, b),1.0);
+    }
+
+}
