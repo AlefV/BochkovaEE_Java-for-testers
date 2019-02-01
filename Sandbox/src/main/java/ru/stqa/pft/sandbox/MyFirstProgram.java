f307e068aee81bf417434504104ac083e1692543
AlefV <planeado@gmail.com>
2019-02-01 13:24:17 +0300

    Функция расстояния между точками

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,16 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Point a = new Point (1,4);
+        Point b = new Point (1,5);
+
+        System.out.println("Расстояние между точками x = "  + a.x + ", y = " + a.y + " и x = " + b.x + ", y = " + b.y + " = " +distance(a,b));
+    }
+
+    public static double distance (Point a, Point b){
+        double xa = (a.x - b.x)*(a.x - b.x);
+        double yb = (a.y - b.y)*(a.y - b.y);
+        double d = Math.sqrt(xa + yb);
+        return d;
     }
 }
