bfd76b519824833918c15101848e57cd61a2c725
AlefV <planeado@gmail.com>
2019-02-01 16:34:42 +0300

    Метод в классе Point

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,13 +6,8 @@ public class MyFirstProgram {
         Point a = new Point (1,4);
         Point b = new Point (1,5);
 
-        System.out.println("Расстояние между точками x = "  + a.x + ", y = " + a.y + " и x = " + b.x + ", y = " + b.y + " = " +distance(a,b));
+        System.out.println("Расстояние между точками x = "  + a.x + ", y = " + a.y + " и x = " + b.x + ", y = " + b.y + " = " +a.distance(a,b));
     }
 
-    public static double distance (Point a, Point b){
-        double xa = (a.x - b.x)*(a.x - b.x);
-        double yb = (a.y - b.y)*(a.y - b.y);
-        double d = Math.sqrt(xa + yb);
-        return d;
-    }
+
 }
--- Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -9,4 +9,12 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+
+    public double distance (Point a, Point b) {
+        double xa = (a.x - b.x) * (a.x - b.x);
+        double yb = (a.y - b.y) * (a.y - b.y);
+        double d = Math.sqrt(xa + yb);
+        return d;
+
+    }
 }
