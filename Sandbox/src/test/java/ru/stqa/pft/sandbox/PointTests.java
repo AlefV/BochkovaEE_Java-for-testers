f884641191ab8b176e68de719bd3d1a9d6b154a5
AlefV <planeado@gmail.com>
2019-02-05 17:13:31 +0300

    Поправлено с учетом исправления метода

--- Sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ Sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -8,13 +8,13 @@ public class PointTests {
     public void pointTest1(){
         Point a = new Point(1,3);
         Point b = new Point (1,4);
-        Assert.assertEquals(a.distance(a, b),1.0);
+        Assert.assertEquals(a.distance(b),1.0);
     }
     @Test
     public void pointTest2(){
         Point a = new Point(1,3);
         Point b = new Point (2,3);
-        Assert.assertEquals(a.distance(a, b),1.0);
+        Assert.assertEquals(a.distance(b),1.0);
     }
 
 }
