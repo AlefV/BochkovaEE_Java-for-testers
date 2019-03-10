a5e72e9ac0a918678eb3c14d6b1a3410a47c7cc0
AlefV <planeado@gmail.com>
2019-03-10 15:20:02 +0300

    Реализован конфигурационный файл

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -11,7 +11,8 @@ import ru.stqa.pft.addressbook.model.UserData;
 
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+    protected static final ApplicationManager app =
+            new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
 
     @BeforeSuite(alwaysRun = true)
     public void setUp() throws Exception {
