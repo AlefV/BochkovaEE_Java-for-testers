90c43f4cf66ccfc9099b62be6a75d73b7982fd15
AlefV <planeado@gmail.com>
2019-03-31 21:07:17 +0300

    Интеграция тестов с багтрекером Mantis

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -23,6 +23,7 @@ public class ApplicationManager {
     private JamesHelper jamesHelper;
     private DbHelper dbHelper;
     private ChangePasswordHelper changePasswordHelper;
+    private SoapHelper soapHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -106,5 +107,12 @@ public class ApplicationManager {
             changePasswordHelper = new ChangePasswordHelper(this);
         }
         return changePasswordHelper;
+    }
+
+    public SoapHelper soap(){
+        if (soapHelper == null){
+            soapHelper = new SoapHelper(this);
+        }
+        return soapHelper;
     }
 }
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -2,11 +2,15 @@ package ru.stqa.pft.mantis.tests;
 
 import org.openqa.selenium.remote.BrowserType;
 
+import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
+import javax.xml.rpc.ServiceException;
 import java.io.File;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
 
 public class TestBase {
 
@@ -19,6 +23,16 @@ public class TestBase {
         app.ftp().upload(new File("src/test/resources/config_inc.php"), "config_inc.php", "config_inc.php.bak");
     }
 
+    boolean isIssueOpen(int issueId) throws RemoteException, ServiceException, MalformedURLException {
+        String status = app.soap().getIssue(issueId).getStatus().getName();
+        return !status.equals("resolved");
+    }
+
+    public void skipIfNotFixed(int issueId) throws RemoteException, ServiceException, MalformedURLException {
+        if (isIssueOpen(issueId)) {
+            throw new SkipException("Ignored because of issue " + issueId);
+        }
+    }
     @AfterSuite(alwaysRun = true)
     public void tearDown() throws Exception {
         app.ftp().restore("config_inc.php.bak", "config_inc.php");
